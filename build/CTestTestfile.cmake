b34281189f49724b75d6cd37a0c9551784e3c5e6
TheCarmelGuy <komelm13@gmail.com>
2023-05-18 01:39:36 -0400

    got rid of build artificats

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/komelmerchant/Projects/glut-demo
-# Build directory: /home/komelmerchant/Projects/glut-demo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
